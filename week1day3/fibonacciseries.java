e2bd4e10c4c77d3211babca7d3b6f90295b635c6
Amarnath-21 <115524106+Amarnath-21@users.noreply.github.com>
2022-10-13 08:43:50 +0530

    Add files via upload

---
+++ week1day3/fibonacciseries.java
@@ -0,0 +1,34 @@
+package week1day3;
+
+public class fibonacciseries {
+	// calling the variable inside the main method so we 
+		//use the globle variable as static
+		static int A = 0;
+		static int B = 1;
+		static int  C;
+		public static void main(String[] args) {
+			System.out.print(A +" "+B);
+			// Range into 8 so we give greater then 9
+			for (int i = 2; i < 9; i++) {
+				
+				//sum the first number and second number
+					C = A+B;
+					System.out.print(" "+C);
+					// swapping the values
+					A=B;
+					B = C; 
+					
+					
+					
+					
+					
+				}
+
+			
+			
+			
+			
+			
+			
+		}
+}	
---
+++ week1day3/twopostiveandnegativevalues.java
@@ -0,0 +1,14 @@
+package week1day3;
+
+public class twopostiveandnegativevalues {
+	public static void main(String[] args) { 
+		 int number= -56; 
+		 if(number > 0){ 
+		  System.out.println("The number is positive"); 
+		  
+		 }else if (number<0){
+			  System.out.println("The number is negative");
+			  }
+		 else {System.out.println("The number is neither positive nor negative");}
+}
+}
